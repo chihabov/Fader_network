6840d8f277a57660a54b7f7963ae3eb5cd832d81
chihabov <94179239+chihabov@users.noreply.github.com>
2022-12-04 23:07:48 +0100

    (affichage)

---
+++ visual.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  4 20:54:34 2022
+
+@author: spi-2019-39
+"""
+import matplotlib.pyplot as plt
+
+def Affichge(x):
+    fig = plt.figure(figsize=(7, 3))
+    nplot = 10
+    for i in range(1, nplot):
+        ax = fig.add_subplot(1, nplot, i)
+        ax.imshow(x[i])
+    plt.show()
