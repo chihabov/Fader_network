e81f6acfbd276a6fe95b26ffc9a21a40c6595084
fenicheSofiane <94178828+fenicheSofiane@users.noreply.github.com>
2022-12-04 20:50:41 +0100

    update main

--- Main.py
+++ Main.py
@@ -19,3 +19,32 @@ from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Flatten, Dense, UpSampling2D, Reshape
 
 
+from TraitementData import *
+
+path_img = "./demo/CelebA_subset/images"
+csv_path = './demo/CelebA_subset/list_attr_celeba.csv'
+
+
+
+dataset = DatasetCelebA(root=path_img, attr=csv_path)
+
+
+NomImg = np.sort(os.listdir(path_img))
+
+print(len(NomImg))
+lenTest, lenTrain = int(len(NomImg) - (len(NomImg) * 0.8)), int(
+    len(NomImg) - (len(
+        NomImg) * 0.2))
+
+NomImgTrain = NomImg[:lenTrain]
+NomImgTest = NomImg[lenTrain:lenTrain + lenTest]
+NomImgAll = NomImg[:]
+
+
+
+X_train = DataTrain(NomImgTrain,path_img)
+print("X_train.shape = {}".format(X_train.shape))
+X_test = DataTrain(NomImgTest,path_img)
+print("X_test.shape = {}".format(X_test.shape))
+# X_all =(tf.data.Dataset.from_tensor_slices(DataTrain(NomImgAll)))
+X_all = tf.convert_to_tensor(DataTrain(NomImgAll,path_img))
