ffa157ba3827d215ebd3b27eb4661ce2ac3666ec
fenicheSofiane <94178828+fenicheSofiane@users.noreply.github.com>
2022-12-20 16:05:59 +0100

    update the AE
    change the model, add compile and update the call fonction

--- model.py
+++ model.py
@@ -54,59 +54,93 @@ class Discriminator(Model):
       
       
 
-class Autoencoder(Model):
-  def __init__(self):
-        super().__init__()
-        #self.embeddings = None
-  def __init__(self):
-    super(Autoencoder, self).__init__()
-    self.encoder = tf.keras.Sequential([
-      layers.Input(shape=(256, 256, 3)),
-      layers.Conv2D(16,kernel_size=4, padding='same', strides=2),
-      layers.BatchNormalization(),
-      layers.LeakyReLU(0.2),
-      layers.Conv2D(32,kernel_size=4, padding='same', strides=2),
-      layers.BatchNormalization(),
-      layers.LeakyReLU(0.2),
-      layers.Conv2D(64,kernel_size=4, padding='same', strides=2),
-      layers.BatchNormalization(),
-      layers.LeakyReLU(0.2),
-      layers.Conv2D(128, kernel_size=4, padding='same', strides=2),
-      layers.BatchNormalization(),
-      layers.LeakyReLU(0.2),      
-      layers.Conv2D(256, kernel_size=4, padding='same', strides=2),     
-      layers.BatchNormalization(),
-      layers.LeakyReLU(0.2),
-      layers.Conv2D(512, kernel_size=4, padding='same',strides=2),      
-      layers.BatchNormalization(),
-      layers.LeakyReLU(0.2),
-      layers.Conv2D(512, kernel_size=4, padding='same', strides=2)])
+class Autoencoder(keras.Model):
 
-    self.decoder = tf.keras.Sequential([
-      layers.Conv2DTranspose(512, kernel_size=4, strides=2,padding='same'),
-      layers.BatchNormalization(),
-      layers.ReLU(),
-      
-      layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding='same'),
-      layers.BatchNormalization(),
-      layers.ReLU(),
-      layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding='same'),
-      layers.BatchNormalization(),
-      layers.ReLU(),      
-      layers.Conv2DTranspose(64, kernel_size=4, strides=2, padding='same'),
-        layers.BatchNormalization(),
-      layers.ReLU(),   
-      layers.Conv2DTranspose(32, kernel_size=4, strides=2, padding='same'),
-      layers.BatchNormalization(),
-      layers.ReLU(),
-      layers.Conv2DTranspose(16, kernel_size=4, strides=2, padding='same'),
-      layers.BatchNormalization(),
-      layers.ReLU(),
-      layers.Conv2DTranspose(3, kernel_size=4, strides=2,padding='same')
-      ])
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.embedding = None
+        self.encoder = tf.keras.Sequential([
+            layers.Input(shape=(256, 256, 3)),
+            layers.Conv2D(16, kernel_size=4, padding='same', strides=2),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(0.2),
+            layers.Conv2D(32, kernel_size=4, padding='same', strides=2),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(0.2),
+            layers.Conv2D(64, kernel_size=4, padding='same', strides=2),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(0.2),
+            layers.Conv2D(128, kernel_size=4, padding='same', strides=2),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(0.2),
+            layers.Conv2D(256, kernel_size=4, padding='same', strides=2),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(0.2),
+            layers.Conv2D(512, kernel_size=4, padding='same', strides=2),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(0.2),
+            layers.Conv2D(512, kernel_size=4, padding='same', strides=2),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(0.2)])
 
-  def call(self, x):
-    encoder=self.encoder(x)
-    decoded = self.decoder(encoder)
-    return decoded
-#tf.concat((self.embeddings, features),axis=1)
+        self.decoder = tf.keras.Sequential([
+            layers.Input(shape=(2,2,512+1)),
+            layers.Conv2DTranspose(512+1, kernel_size=4, strides=2,
+                                   padding='same'),
+            layers.BatchNormalization(),
+            layers.ReLU(),
+
+            layers.Conv2DTranspose(256 + 1, kernel_size=4, strides=2,
+                                   padding='same'),
+            layers.BatchNormalization(),
+            layers.ReLU(),
+
+            layers.Conv2DTranspose(128 + 1, kernel_size=4, strides=2,
+                                   padding='same'),
+            layers.BatchNormalization(),
+            layers.ReLU(),
+
+            layers.Conv2DTranspose(64 + 1, kernel_size=4, strides=2,
+                                   padding='same'),
+            layers.BatchNormalization(),
+            layers.ReLU(),
+            layers.Conv2DTranspose(32 + 1, kernel_size=4, strides=2,
+                                   padding='same'),
+            layers.BatchNormalization(),
+            layers.ReLU(),
+            layers.Conv2DTranspose(16 + 1, kernel_size=4, strides=2,
+                                   padding='same'),
+            layers.BatchNormalization(),
+            layers.ReLU(),
+            layers.Conv2DTranspose(3, kernel_size=4, strides=2,
+                                   padding='same'),
+            layers.BatchNormalization(),
+            layers.ReLU(),
+        ])
+
+
+    def compile(self,auto_opti,AE_l = keras.losses.MeanSquaredError()):
+
+        self.AE_loss = AE_l
+        self.g_opti = auto_opti
+        self.encoder.compile(
+            self.g_opti,
+            self.AE_loss)
+
+    """def build(self,learning_rate=0.002,beta_1=0.5,
+                                        name='Adam'):
+           self.lr=learning_rate
+           self.b1=beta_1
+           self.name=name
+           self.encoder.build(self.lr,self.b1,self.name)"""
+
+
+    def call(self, input):
+        x, features = input
+        embedding = self.encoder(x)
+        decoded = self.decoder(tf.concat((embedding, features),
+                                         axis=3))
+        return  decoded,embedding
+
+
+
