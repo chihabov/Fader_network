2f9a75c25f7ecfbdc958d7de819b2ab90fd873b2
Zak1105 <94179384+Zak1105@users.noreply.github.com>
2022-12-22 10:34:44 +0100

    Update latest model with step train and test_step

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-
+from natsort import natsorted, ns
 import csv
 import os
 from abc import ABC
@@ -16,131 +16,90 @@ import pandas as pd
 from PIL import Image
 import tensorflow_datasets as tfds
 
+class Autoencoder(Model):
 
 
-
-class Discreminateur(keras.Model):
-
-    def __init__(self):
-        super(Discreminateur, self).__init__()
-        self.outD = None
-
-        self.discriminator = tf.keras.Sequential([
-            layers.Input(shape=(2, 2, 512)),
-            layers.Conv2DTranspose(512, kernel_size=4, strides=1,padding='same'),
-            layers.Flatten(),
-            layers.Dropout(0.3),
-            keras.layers.Dense(256, input_shape=(512,), activation=None),
-            keras.layers.Dense(4,input_shape=(512,)),
-            ])
-
-
-    def compile(self, disc_opti,d_loss =keras.losses.BinaryCrossentropy(from_logits=True)):
+  def __init__(self,encoder, decoder, discriminator):
+    super(Autoencoder, self).__init__()
+    self.encod=encoder
+    self.decod=decoder
+    self.disc=discriminator
+  def compile(self, disc_opti, auto_opti):
+        super(Autoencoder, self).compile()
         self.d_opti = disc_opti
-        self.Disc_loss = d_loss
-
-        self.discriminator.compile(self.d_opti,self.Disc_loss)
-    """def build(self,learning_rate=0.002,beta_1=0.5,
-                                        name='Adam'):
-           self.lr=learning_rate
-           self.b1=beta_1
-           self.name=name
-           self.discriminator.build(self.lr,self.b1,self.name)"""
-
-    def call(self, embedding):
-
-        self.outD =tf.sigmoid(self.discriminator(embedding))
-        return  self.outD
-      
-      
-
-class Autoencoder(keras.Model):
-
-    def __init__(self):
-        super(Autoencoder, self).__init__()
-        self.embedding = None
-        self.encoder = tf.keras.Sequential([
-            layers.Input(shape=(256, 256, 3)),
-            layers.Conv2D(16, kernel_size=4, padding='same', strides=2),
-            layers.BatchNormalization(),
-            layers.LeakyReLU(0.2),
-            layers.Conv2D(32, kernel_size=4, padding='same', strides=2),
-            layers.BatchNormalization(),
-            layers.LeakyReLU(0.2),
-            layers.Conv2D(64, kernel_size=4, padding='same', strides=2),
-            layers.BatchNormalization(),
-            layers.LeakyReLU(0.2),
-            layers.Conv2D(128, kernel_size=4, padding='same', strides=2),
-            layers.BatchNormalization(),
-            layers.LeakyReLU(0.2),
-            layers.Conv2D(256, kernel_size=4, padding='same', strides=2),
-            layers.BatchNormalization(),
-            layers.LeakyReLU(0.2),
-            layers.Conv2D(512, kernel_size=4, padding='same', strides=2),
-            layers.BatchNormalization(),
-            layers.LeakyReLU(0.2),
-            layers.Conv2D(512, kernel_size=4, padding='same', strides=2),
-            layers.BatchNormalization(),
-            layers.LeakyReLU(0.2)])
-
-        self.decoder = tf.keras.Sequential([
-            layers.Input(shape=(2,2,512+1)),
-            layers.Conv2DTranspose(512+1, kernel_size=4, strides=2,
-                                   padding='same'),
-            layers.BatchNormalization(),
-            layers.ReLU(),
-
-            layers.Conv2DTranspose(256 + 1, kernel_size=4, strides=2,
-                                   padding='same'),
-            layers.BatchNormalization(),
-            layers.ReLU(),
-
-            layers.Conv2DTranspose(128 + 1, kernel_size=4, strides=2,
-                                   padding='same'),
-            layers.BatchNormalization(),
-            layers.ReLU(),
-
-            layers.Conv2DTranspose(64 + 1, kernel_size=4, strides=2,
-                                   padding='same'),
-            layers.BatchNormalization(),
-            layers.ReLU(),
-            layers.Conv2DTranspose(32 + 1, kernel_size=4, strides=2,
-                                   padding='same'),
-            layers.BatchNormalization(),
-            layers.ReLU(),
-            layers.Conv2DTranspose(16 + 1, kernel_size=4, strides=2,
-                                   padding='same'),
-            layers.BatchNormalization(),
-            layers.ReLU(),
-            layers.Conv2DTranspose(3, kernel_size=4, strides=2,
-                                   padding='same'),
-            layers.BatchNormalization(),
-            layers.ReLU(),
-        ])
-
-
-    def compile(self,auto_opti,AE_l = keras.losses.MeanSquaredError()):
-
-        self.AE_loss = AE_l
+        self.Disc_loss = keras.losses.BinaryCrossentropy(from_logits = True)
+        self.AE_loss = keras.losses.MeanSquaredError()
         self.g_opti = auto_opti
-        self.encoder.compile(
+        self.disc.compile(
+            self.d_opti,
+            self.Disc_loss)
+        self.decod.compile(
             self.g_opti,
             self.AE_loss)
+        
+  def train_step(self,data_batch,attr_batch,features,l=0):
+            self.data_batch=data_batch
+            self.attr_batch=attr_batch
+            self.l=l
+            self.features=features
+        
+            img=self.data_batch
+            img=tf.Variable(img)
 
-    """def build(self,learning_rate=0.002,beta_1=0.5,
-                                        name='Adam'):
-           self.lr=learning_rate
-           self.b1=beta_1
-           self.name=name
-           self.encoder.build(self.lr,self.b1,self.name)"""
-
-
-    def call(self, input):
-        x, features = input
-        embedding = self.encoder(x)
-        decoded = self.decoder(tf.concat((embedding, features),
-                                         axis=3))
-        return  decoded,embedding
+            self.disc.trainable = True
+            self.decod.trainable = False            
+           
+            with tf.GradientTape() as Tape:
+                    ez=self.encod(img)
+                    outdiscriminator = self.disc(ez)
+                    #print("taille sortie discriminator",outdiscriminator.shape)
+                    #print("real attr_batch",self.features.shape)
+                    D_loss=self.Disc_loss(self.attr_batch,outdiscriminator)
+                    #print(D_loss)
+                    D_loss2=self.Disc_loss(1-self.attr_batch,outdiscriminator)
+                    #print(D_loss2)
+                    
+            gradient_rec = Tape.gradient(D_loss, self.disc.trainable_weights)
+            self.d_opti.apply_gradients(zip(gradient_rec, self.disc.trainable_weights))                         
+            
+            #gradient_diss = Tape.gradient(D_loss, AE.discriminator.trainable_weights)
+            #AE.discriminator.optimizer.apply_gradients(zip(gradient_diss, AE.discriminator.trainable_weights))
+            self.disc.trainable = False
+            self.decod.trainable = True
+            self.encod.trainable=True
+            #img.features=input
+            img=tf.Variable(img)
+            with tf.GradientTape() as Tape:
+                ez=self.encod(img)
+                inp=tf.concat((ez, self.features),axis=3)
+                x_reconstruct=self.decod(inp)
+                outdiscrim = self.disc(ez)
+                flipt_attr = 1 - self.attr_batch
+                loss_reconstruct = self.AE_loss(img,x_reconstruct)
+                #print("losss_reconstruct",loss_reconstruct)
+                loss_model = loss_reconstruct + self.l*self.Disc_loss(flipt_attr, outdiscrim)
+    
+            gradient_rec = Tape.gradient(loss_model, self.decod.trainable_weights)
+            self.g_opti.apply_gradients(zip(gradient_rec, self.decod.trainable_weights))  
+            
+            return D_loss,  loss_model
+  def test_step(self,data_batch,attr_batch,features,l=0):
+      
+        self.data_batch=data_batch
+        self.attr_batch=attr_batch
+        self.l=l
+        self.features=features
+        img=self.data_batch
+        img=tf.Variable(img)
+        ez=self.encod(img)
+        inp=tf.concat((ez, self.features),axis=3)
+        x_reconstruct=self.decod(inp)
+        outdiscrim = self.disc(ez)
+        D_loss=self.Disc_loss(self.attr_batch,outdiscrim)
+        flipt_attr = 1 - self.attr_batch
+        loss_reconstruct = self.AE_loss(img,x_reconstruct)
+        loss_model = loss_reconstruct + self.l*self.Disc_loss(flipt_attr, outdiscrim)
+        return D_loss, loss_model
 
 
 
